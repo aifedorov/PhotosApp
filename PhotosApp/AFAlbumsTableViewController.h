1daabf7d2ab2342b99126f0d9adb53049630a741
alexander <fairk19@gmail.com>
2016-03-29 17:31:29 +0300

    Добавлен класс AFAlbumsTableViewController для списка альбомов.

---
+++ PhotosApp/AFAlbumsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AFAlbumsTableViewController.h
+//  PhotosApp
+//
+//  Created by Александр on 29.03.16.
+//  Copyright © 2016 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFAlbumsTableViewController : UITableViewController
+
+@end
